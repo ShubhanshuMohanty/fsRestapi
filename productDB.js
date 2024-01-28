ad327c400bf32dc5b2fa19296d1de0cb0a049726
Shubhanshu Mohanty <shubhanshumohanty@gmail.com>
2024-01-28 20:54:33 +0530

    stored api data json

---
+++ productDB.js
@@ -0,0 +1,16 @@
+const connectDB=require('./db/connect.js');
+require('dotenv').config();
+const Product=require('./models/product.js');
+const ProductJson=require('./products.json');
+
+const start=async()=>{
+    try {
+        await connectDB(process.env.MONGODB_URL)
+        await Product.create(ProductJson);
+        console.log("success");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();

33c9e55a649769660e373b05b700b7cab0a670ea
Shubhanshu Mohanty <shubhanshumohanty@gmail.com>
2024-01-29 20:35:23 +0530

    add sort functuionality

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product=require('../models/product.js')
 
 const getAllProducts=async(req,res)=>{
 
-    const {company,name,featured}=req.query;
+    const {company,name,featured,sort}=req.query;
     const queryObject={};
 
     if (company) {
@@ -18,14 +18,22 @@ const getAllProducts=async(req,res)=>{
         queryObject.name={$regex: name, $options:"i"};
     }
 
+    let apiData=Product.find(queryObject);
+    
+    if (sort) {
+        let sortFix=sort.replace(',',' ');
+        // queryObject.sort=sortFix;
+        apiData=apiData.sort(sortFix)
+    }
+
     console.log(queryObject);
 
-    const myData=await Product.find(queryObject);
+    const myData=await apiData;
     res.status(200).json({myData});
 }
 
 const getAllProductsTesting=async(req,res)=>{
-    const myData=await Product.find(req.query);
+    const myData=await Product.find(req.query).sort("name -price");
     //?company=mi iske liye req.query
     //http://localhost:5000/api/products/testing?company=mi
     //for many field      ?company=apple&name=iphone

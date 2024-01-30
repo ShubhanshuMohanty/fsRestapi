7eccf777df69f999aa6abc6de5c13335fe5aaefa
Shubhanshu Mohanty <shubhanshumohanty@gmail.com>
2024-01-30 10:23:22 +0530

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -34,13 +34,21 @@ const getAllProducts=async(req,res)=>{
         
     }
 
+    let page=Number(req.query.page) || 1;
+    let limit=Number(req.query.limit) || 10;
+
+    let skip=(page-1)*limit;
+
+    apiData=apiData.skip(skip).limit(limit);
+
     // console.log(queryObject);
 
     const myData=await apiData;
-    res.status(200).json({myData});
+    res.status(200).json({myData,nbHits: myData.length});
 }
 
 const getAllProductsTesting=async(req,res)=>{
+    console.log(req.query);
     const myData=await Product.find(req.query).select("name");
     //?company=mi iske liye req.query
     //http://localhost:5000/api/products/testing?company=mi
